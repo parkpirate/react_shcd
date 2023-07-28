c1007bb24925cfe622f5ac826866c3d0e6a39d2b
parkpirate <skbsy153@gmail.com>
2023-07-28 17:49:45 +0900

    4_component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+	return <header>
+			<h1><a href="/">WEB</a></h1>
+		</header>
+}
+
+function Nav() {
+	return <nav>
+		<ol>
+			<li><a href="/read/1">Html</a></li>
+			<li><a href="/read/2">Css</a></li>
+			<li><a href="/read/3">Java Script</a></li>
+		</ol>
+	</nav>
+}
+
+function Article() {
+	return <article>
+		<h2>Welcome</h2>
+		Hello, WEB
+	</article>
+}
+
 function App() {
   return (
-    <div className="App">
-      Hello react!
+    <div>
+      {/* home으로 이동하는 header 영역 */}
+      <Header></Header>
+
+			{/* 구체적인 글을 보는 페이지로 이동하는 영역 */}
+      <Nav></Nav>
+
+			{/* 본문 영역 */}
+			<Article></Article>
     </div>
   );
 }
