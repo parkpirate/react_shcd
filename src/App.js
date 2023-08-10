705f0a0e0a50a69be548c08728d0e7201172d8b9
parkpirate <skbsy153@gmail.com>
2023-08-10 19:23:33 +0900

    7. state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function Header(props) {
 	console.log('props', props, props.title);
@@ -20,7 +21,7 @@ function Nav(props) {
 		lis.push(<li key={t.id}>
 			<a id={t.id} href={'/read/'+t.id} onClick={event=>{
 				event.preventDefault();
-				props.onChangeMode(event.target.id);
+				props.onChangeMode(Number(event.target.id));
 			}}>{t.title}</a>
 		</li>)
 	}
@@ -41,26 +42,46 @@ function Article(props) {
 
 function App() {
 
+	const [mode, setMode] = useState('WELCOME');
+	const [id, setId] = useState(null);
+
 	const topics = [
 		{id:1, title:'Html', body:'html is ...'},
 		{id:2, title:'Css', body:'css is ...'},
 		{id:3, title:'JavaScript', body:'javascript is ...'}
 	]
 
+	let content = null;
+
+	if (mode === 'WELCOME') {
+		content = <Article title="Welcome" body="Hello, WEB"></Article>
+	} else if (mode === 'READ') {
+		let title, body = null;
+		for(let i=0; i<topics.length; i++) {
+			console.log(topics[i].id, id);
+			if(topics[i].id === id) {
+				title = topics[i].title;
+				body = topics[i].body;
+			}
+		}
+		content = <Article title={title} body={body}></Article>
+	}
+
   	return (
     <div>
     	{/* home으로 이동하는 header 영역 */}
     	<Header title="WEB" onChangeMode={()=>{
-			alert('Header');
+			setMode('WELCOME');
 		}}></Header>
 
 		{/* 구체적인 글을 보는 페이지로 이동하는 영역 */}
-    	<Nav topics={topics} onChangeMode={function(id){
-			alert(id);
+    	<Nav topics={topics} onChangeMode={(_id)=>{
+			setMode('READ');
+			setId(_id);
 		}}></Nav>
 
 		{/* 본문 영역 */}
-		<Article title="Welcome" body="Hello, WEB"></Article>
+		{content}
     </div>
   );
 }
