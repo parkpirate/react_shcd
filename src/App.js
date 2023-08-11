5b2940fa6ec34dd1e95feb605e9faa60b500b92f
parkpirate <skbsy153@gmail.com>
2023-08-11 18:06:08 +0900

    8. Create

--- src/App.js
+++ src/App.js
@@ -40,16 +40,33 @@ function Article(props) {
 	</article>
 }
 
+function Create(props) {
+	return <article>
+		<h2>Create</h2>
+		<form onSubmit={event=>{
+			event.preventDefault();
+			const title = event.target.title.value;
+			const body = event.target.body.value;
+			props.onCreate(title, body);
+		}}>
+			<p><input type="text" name="title" placeholder="title"/></p>
+			<p><textarea name="body" placeholder="body"></textarea></p>
+			<p><input type="submit" value="Create"></input></p>
+		</form>
+	</article>
+}
+
 function App() {
 
 	const [mode, setMode] = useState('WELCOME');
 	const [id, setId] = useState(null);
+	const [nextId, setNextId] = useState(4);
 
-	const topics = [
+	const [topics, setTopics] = useState([
 		{id:1, title:'Html', body:'html is ...'},
 		{id:2, title:'Css', body:'css is ...'},
 		{id:3, title:'JavaScript', body:'javascript is ...'}
-	]
+	]);
 
 	let content = null;
 
@@ -65,6 +82,16 @@ function App() {
 			}
 		}
 		content = <Article title={title} body={body}></Article>
+	} else if (mode === 'CREATE') {
+		content = <Create onCreate={(_title, _body)=>{
+			const newTopic = {id:nextId, title:_title, body:_body}
+			const newTopics = [...topics]
+			newTopics.push(newTopic);
+			setTopics(newTopics);
+			setMode('READ');
+			setId(nextId);
+			setNextId(nextId+1);
+		}}></Create>
 	}
 
   	return (
@@ -82,6 +109,12 @@ function App() {
 
 		{/* 본문 영역 */}
 		{content}
+
+		<a href="/create" onClick={event=>{
+			event.preventDefault();
+			setMode('CREATE');
+		}}>Create</a>
+
     </div>
   );
 }
